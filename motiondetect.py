3126cfa548b601f86144bc5f5bca6bc42010daaf
VEDANT-CPU <vedantpawar896@gmail.com>
2025-07-28 18:01:26 +0530

    new file for plotting

--- motiondetect.py
+++ motiondetect.py
@@ -39,6 +39,7 @@ while True:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
 
     status_list.append(status)
+    status_list=status_list[-2:]
     #we put if below as we want to check transition after latest status appended.
     if status_list[-1]==1 and status_list[-2]==0:
         times.append(datetime.now())
@@ -63,6 +64,7 @@ print(status_list)
 print(times)
 
 j=0
+#To append rows in the dataframe
 for i in range(0,len(times),2):
     #dfs=pandas.DataFrame({"Start":times[i],"End":times[i+1]},index=[0])
     #df=df.concat([dfs],ignore_index=True)
