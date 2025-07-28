56463ba91e0d12a0c9941d83c3f4fadb0b7522f7
VEDANT-CPU <vedantpawar896@gmail.com>
2025-07-28 19:31:10 +0530

    2nd Plot

--- plotting.py
+++ plotting.py
@@ -7,8 +7,11 @@ from bokeh.plotting import figure, output_file, show
 p=figure(x_axis_type="datetime", width=500, height=400)
 p.title.text="Motion Graph"
 p.sizing_mode="scale_width"
+p.yaxis.minor_tick_line_color=None
+#To remove the horizontal grid lines from the graph, This is a trick.
+p.ygrid.ticker=[0,1] #Pass a list of desired tickers
 
 q=p.quad(left=df["Start"], right=df["End"], bottom=0, top=1, color="red")
 
-output_file("TimePlot.html")
+output_file("TimePlot2.html")
 show(p)
